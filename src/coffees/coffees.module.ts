a5496993734c2a26b142582efacdd95d8c691bfe
geraldo.braz <gban50@gmail.com>
2023-10-06 11:20:15 -0400

    Application Configuration

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,17 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity/event.entity';
-
-export class MockCoffeesService {}
+import { ConfigModule } from '@nestjs/config';
+import coffeesConfig from './config/coffees.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // 👈 Adding Coffee Entity here to TypeOrmModule.forFeature
-  controllers: [CoffeesController],
-  providers: [
-    {
-      provide: CoffeesService,
-      useValue: new MockCoffeesService(), // <-- mock implementation
-    },
+  imports: [
+    TypeOrmModule.forFeature([Coffee, Flavor, Event]),
+    ConfigModule.forFeature(coffeesConfig),
   ],
+  controllers: [CoffeesController],
+  providers: [CoffeesService],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DataSource, Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
@@ -7,6 +7,8 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 import { Event } from '../events/entities/event.entity/event.entity';
+import { ConfigType } from '@nestjs/config';
+import coffeesConfig from './config/coffees.config';
 
 @Injectable()
 export class CoffeesService {
@@ -16,7 +18,16 @@ export class CoffeesService {
     @InjectRepository(Flavor)
     private readonly flavorRepository: Repository<Flavor>,
     private readonly dataSource: DataSource,
-  ) {}
+    // private readonly configService: ConfigService,
+    @Inject(coffeesConfig.KEY)
+    private coffeesConfiguration: ConfigType<typeof coffeesConfig>,
+  ) {
+    // const databaseHost = this.configService.get('DATABASE_HOST', 'localhost');
+    // const databaseHost = this.configService.get('database.host', 'localhost');
+    // console.log(databaseHost);
+
+    console.log(coffeesConfiguration.foo);
+  }
 
   findAll({ limit, offset }: PaginationQueryDto) {
     return this.coffeeRepository.find({
